872aed695fe8cb81e2c14c81644ef249b377fd23
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-05-18 14:58:54 +0800

    update

--- hr_promotion/models/employee_promotion.py
+++ hr_promotion/models/employee_promotion.py
@@ -15,6 +15,7 @@ class EmployeePromotion(models.Model):
     name = fields.Char(string='Promotion', required=True, help='the promotion name')
     previous_salary = fields.Integer(string='Previous Salary',  help='the previous salary')
     new_salary = fields.Integer(string='New Salary',  help='the new salary')
+    adjustment = fields.Integer(string='Salary Adjustment',  help='the adjustment salary')
     previous_job_position = fields.Many2one('hr.job', string='Previous Job Postion',  Help='Previous job position')
     new_job_position = fields.Many2one('hr.job', string='New Job Postion',  Help='New job position')
     date = fields.Date(string='Date',
@@ -35,11 +36,8 @@ class HrPromotion(models.Model):
             ins_amount = 0
             ins_date = time.strftime('%Y-%m-%d')
             for ins in emp.promotion:
-                ins_amount = ins_amount + ins.new_salary
+                ins_amount = ins_amount + ins.adjustment
                 ins_date = ins.date
-
-
-
         emp.promotion_salary = ins_amount
         emp.promotion_date = ins_date
 
